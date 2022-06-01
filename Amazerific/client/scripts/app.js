5c0fff6c03ff0524ed747c2b0bc2bc50475a78c4
MemeDeviluke <mars3l.valeev@yandex.ru>
2022-06-01 15:21:55 +0300

    Объединение функции удаление и редактирования

--- Amazerific/client/scripts/app.js
+++ Amazerific/client/scripts/app.js
@@ -31,37 +31,15 @@ var organizeByTags = function (toDoObjects) {
 
 var liaWithEditOrDeleteOnClick = function (todo) {
 	var $todoListItem = $("<li>").text(todo.description),
+		$todoEditLink = $("<a>").attr("href", "todos/" + todo._id),
 		$todoRemoveLink = $("<a>").attr("href", "todos/" + todo._id);
 
-		$todoRemoveLink.text(" Удалить");
-		console.log("todo._id: " + todo._id);
-		console.log("todo.description: " + todo.description);
-	$todoRemoveLink.on("click", function () {
-		$.ajax({
-			url: "/todos/" + todo._id,
-			type: "DELETE",
-			dataType: 'jsonp',
-      		jsonp: 'jsonp'
-		}).done(function (responde) {
-			$(".tabs a:first-child span").trigger("click");
-		}).fail(function (err) {
-			console.log("error on delete 'todo'!");
-		});
-		return false;
-	});
-	$todoListItem.append($todoRemoveLink);
-	return $todoListItem;
-}
-
-var liaWithEditOnClick = function (todo) {
-	var $todoListItem = $("<li>").text(todo.description),
-		$todoRemoveLink = $("<a>").attr("href", "todos/" + todo._id);
-	$todoRemoveLink.text("Редактировать");
-	$todoRemoveLink.on("click", function() {
+		$todoEditLink.text("Редактировать");
+	$todoEditLink.on("click", function() {
 		var newDescription = prompt("Введите новое наименование для задачи", todo.description);
 		if (newDescription !== null && newDescription.trim() !== "") {
 			$.ajax({
-				"url": "/todos.json/" + todo._id,
+				"url": "/todos/" + todo._id,
 				"type": "PUT",
 				"data": { "description": newDescription },
 				"dataType": 'jsonp',
